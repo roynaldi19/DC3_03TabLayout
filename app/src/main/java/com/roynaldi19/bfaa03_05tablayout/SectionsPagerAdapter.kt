06995c2ddd10c9b90e5485875768e593f7d65a79
roynaldi19 <roynaldi19@gmail.com>
2022-01-30 20:24:29 +0700

    Initial commit

---
+++ app/src/main/java/com/roynaldi19/bfaa03_05tablayout/SectionsPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.roynaldi19.bfaa03_05tablayout
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SectionsPagerAdapter(activity: AppCompatActivity): FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int {
+        return 2
+
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when(position) {
+            0 -> fragment = HomeFragment()
+            1 -> fragment = ProfileFragment()
+        }
+        return fragment as Fragment
+
+    }
+}
